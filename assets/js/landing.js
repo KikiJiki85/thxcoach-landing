961aeb60f7f5374e49bd7efc2acd73d23a5d19db
Aleksey Vaskin <alzor85@gmail.com>
2024-01-16 14:50:50 +0700

    feat: add menu change lng

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,10 +1,12 @@
-import setHeaderHandlers from "./header.js";
-import setDashboardHandlers from "./dashboard.js";
-
 function init() {
-    setHeaderHandlers();
-    setDashboardHandlers();
+  const languageSwitch = document.querySelector(".landing-header__right-side");
+  const languageFi = document.querySelector(".landing-header__link-fi");
+  const languageEn = document.querySelector(".landing-header__link-en");
+  languageSwitch.addEventListener("click", () => {
+    languageFi.classList.toggle("landing-header__link-fi--active");
+    languageEn.classList.toggle("landing-header__link-en--active");
+    languageSwitch.classList.toggle("landing-header__right-side--active");
+  });
 }
 
-
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
